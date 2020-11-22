06b6ca64a12165f3e73213ee78e3849bc8ec0121
Aleksandr Endovitskii <endovitsky.a.p@gmail.com>
2020-11-23 02:45:51 +0300

    Add default implementation of GameManager

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -4,6 +4,32 @@ namespace Managers
 {
     public class GameManager : MonoBehaviour
     {
+        // static instance of GameManager which allows it to be accessed by any other script
+        public static GameManager Instance;
 
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+
+                DontDestroyOnLoad(gameObject); // sets this to not be destroyed when reloading scene
+            }
+            else
+            {
+                if (Instance != this)
+                {
+                    // this enforces our singleton pattern, meaning there can only ever be one instance of a GameManager
+                    Destroy(gameObject);
+                }
+            }
+
+            Initialize();
+        }
+
+        public void Initialize()
+        {
+
+        }
     }
 }
