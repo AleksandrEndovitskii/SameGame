2edc8df1de8832f442548ef48b3059f1738b7a66
Aleksandr Endovitckij <endovitsky.a.p@gmail.com>
2021-05-11 19:00:22 +0300

    Add ability to create SquareView to GameObjectsManager

--- Assets/Scripts/Managers/GameObjectsManager.cs
+++ Assets/Scripts/Managers/GameObjectsManager.cs
@@ -1,15 +1,25 @@
 ﻿using UnityEngine;
 using Utils;
+using Views;
 
 namespace Managers
 {
     public class GameObjectsManager : MonoBehaviour, IInitilizable, IUnInitializeble
     {
+        [SerializeField]
+        private SquareView _squareViewPrefab;
+
         public void Initialize()
         {
         }
         public void UnInitialize()
         {
+        }
+
+        public SquareView CreateSquare()
+        {
+            var squareViewInstance = Instantiate(_squareViewPrefab);
+            return squareViewInstance;
         }
     }
 }
