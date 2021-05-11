606dca77f30262f788837f72b88a6a93278055d0
Aleksandr Endovitckij <endovitsky.a.p@gmail.com>
2021-05-11 18:04:35 +0300

    Add creating of BoardModel in BoardManager

--- Assets/Scripts/Managers/BoardManager.cs
+++ Assets/Scripts/Managers/BoardManager.cs
@@ -5,9 +5,16 @@ namespace Managers
 {
     public class BoardManager : MonoBehaviour, IInitilizable
     {
+        [SerializeField]
+        private int _rowsCount;
+        [SerializeField]
+        private int _cellsCount;
+
+        private BoardModel _boardModel;
+
         public void Initialize()
         {
-
+            _boardModel = new BoardModel(_rowsCount, _cellsCount);
         }
     }
 }
