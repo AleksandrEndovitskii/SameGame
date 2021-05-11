db76966d73f48a88b2d6c5b23c17b46e99d2ed14
Aleksandr Endovitckij <endovitsky.a.p@gmail.com>
2021-05-11 18:58:28 +0300

    Add GameObjectsManager

---
+++ Assets/Scripts/Managers/GameObjectsManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using Utils;
+
+namespace Managers
+{
+    public class GameObjectsManager : MonoBehaviour, IInitilizable, IUnInitializeble
+    {
+        public void Initialize()
+        {
+        }
+        public void UnInitialize()
+        {
+        }
+    }
+}
