e0da07c98b0071725940adcbc7196eb31f0de3a5
Aleksandr Endovitckij <endovitsky.a.p@gmail.com>
2021-05-11 18:04:17 +0300

    Add BoardModel

---
+++ Assets/Scripts/Models/BoardModel.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+
+public class BoardModel
+{
+	private readonly List<List<Square>> _squares;
+
+	public BoardModel(int rowsCount, int cellsCount)
+	{
+		_squares = new List<List<Square>>();
+		PlaceSquaresOnBoard(_squares, rowsCount, cellsCount);
+	}
+
+	private void PlaceSquaresOnBoard(List<List<Square>> squares, int rowsCount, int cellsCount)
+	{
+		for (var i = 0; i < rowsCount; i++)
+		{
+			var squaresRow = new List<Square>();
+			squares.Add(squaresRow);
+			for (var j = 0; j < cellsCount; j++)
+			{
+				squares[i].Add(new Square());
+			}
+		}
+	}
+}
