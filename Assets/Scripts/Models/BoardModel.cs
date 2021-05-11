6bedcc4ef590a0416289a77d552cf267dd1fd7be
Aleksandr Endovitckij <endovitsky.a.p@gmail.com>
2021-05-11 19:13:29 +0300

    Add SquareModel

--- Assets/Scripts/Models/BoardModel.cs
+++ Assets/Scripts/Models/BoardModel.cs
@@ -4,25 +4,25 @@ namespace Models
 {
 	public class BoardModel
 	{
-		public List<List<Square>> Squares => _squares;
+		public List<List<SquareModel>> Squares => _squares;
 
-		private readonly List<List<Square>> _squares;
+		private readonly List<List<SquareModel>> _squares;
 
 		public BoardModel(int rowsCount, int cellsCount)
 		{
-			_squares = new List<List<Square>>();
+			_squares = new List<List<SquareModel>>();
 			PlaceSquaresOnBoard(_squares, rowsCount, cellsCount);
 		}
 
-		private void PlaceSquaresOnBoard(List<List<Square>> squares, int rowsCount, int cellsCount)
+		private void PlaceSquaresOnBoard(List<List<SquareModel>> squares, int rowsCount, int cellsCount)
 		{
 			for (var i = 0; i < rowsCount; i++)
 			{
-				var squaresRow = new List<Square>();
+				var squaresRow = new List<SquareModel>();
 				squares.Add(squaresRow);
 				for (var j = 0; j < cellsCount; j++)
 				{
-					squares[i].Add(new Square());
+					squares[i].Add(new SquareModel());
 				}
 			}
 		}
