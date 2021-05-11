8e533da59a444b1ad15683a1737a8ff4be6a0462
Aleksandr Endovitckij <endovitsky.a.p@gmail.com>
2021-05-11 20:35:11 +0300

    Add linking of connected squares

--- Assets/Scripts/Models/BoardModel.cs
+++ Assets/Scripts/Models/BoardModel.cs
@@ -12,6 +12,7 @@ namespace Models
 		{
 			_squares = new List<List<SquareModel>>();
 			PlaceSquaresOnBoard(_squares, rowsCount, cellsCount);
+			LinkSquaresOnBoard(_squares);
 		}
 
 		private void PlaceSquaresOnBoard(List<List<SquareModel>> squares, int rowsCount, int cellsCount)
@@ -23,6 +24,32 @@ namespace Models
 				for (var j = 0; j < cellsCount; j++)
 				{
 					squares[i].Add(new SquareModel());
+				}
+			}
+		}
+		private void LinkSquaresOnBoard(List<List<SquareModel>> squareModels)
+		{
+			for (var i = 0; i < squareModels.Count; i++)
+			{
+				for (var j = 0; j < squareModels[i].Count; j++)
+				{
+					if (i != 0)
+					{
+						squareModels[i][j].Top = squareModels[i - 1][j];
+					}
+					if (i != squareModels[i].Count - 1)
+					{
+						squareModels[i][j].Bot = squareModels[i + 1][j];
+					}
+
+					if (j != 0)
+					{
+						squareModels[i][j].Left = squareModels[i][j - 1];
+					}
+					if (j != squareModels[i].Count - 1)
+					{
+						squareModels[i][j].Right = squareModels[i][j + 1];
+					}
 				}
 			}
 		}
--- Assets/Scripts/Models/SquareModel.cs
+++ Assets/Scripts/Models/SquareModel.cs
@@ -2,5 +2,13 @@ namespace Models
 {
     public class SquareModel
     {
+        public SquareModel Top;
+        public SquareModel Right;
+        public SquareModel Left;
+        public SquareModel Bot;
+
+        public SquareModel()
+        {
+        }
     }
 }
