e99f1a8b0c08abbae9d689af4ca31a4eeeb7c1e8
Aleksandr Endovitckij <endovitsky.a.p@gmail.com>
2021-05-11 17:49:41 +0300

    Add initialisation of UserInterfaceManager

--- Assets/Scripts/Managers/UserInterfaceManager.cs
+++ Assets/Scripts/Managers/UserInterfaceManager.cs
@@ -1,13 +1,31 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
 using Utils;
 
 namespace Managers
 {
-    public class UserInterfaceManager : MonoBehaviour, IInitilizable
+    public class UserInterfaceManager : MonoBehaviour, IInitilizable, IUnInitializeble
     {
+        [SerializeField]
+        private Canvas _canvasPrefab;
+        [SerializeField]
+        private EventSystem _eventSystemPrefab;
+
+        [NonSerialized]
+        public Canvas CanvasInstance;
+        [NonSerialized]
+        public EventSystem EventSystemInstance;
+
         public void Initialize()
         {
-
+            CanvasInstance = Instantiate(_canvasPrefab);
+            EventSystemInstance = Instantiate(_eventSystemPrefab);
+        }
+        public void UnInitialize()
+        {
+            Destroy(EventSystemInstance);
+            Destroy(CanvasInstance);
         }
     }
 }
