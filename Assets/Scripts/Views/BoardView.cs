3702a0fade02e2f79a164a9d3f5cc26c3f34295d
Aleksandr Endovitckij <endovitsky.a.p@gmail.com>
2021-05-11 19:02:43 +0300

    Add board

---
+++ Assets/Scripts/Views/BoardView.cs
@@ -0,0 +1,21 @@
+using Components;
+
+namespace Views
+{
+    public class BoardView : BaseComponent
+    {
+        protected override void Initialize()
+        {
+        }
+        protected override void UnInitialize()
+        {
+        }
+
+        protected override void Subscribe()
+        {
+        }
+        protected override void UnSubscribe()
+        {
+        }
+    }
+}
