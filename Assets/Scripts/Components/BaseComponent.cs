826155e8a0c97b673c1838e53e40ccca9b35523b
Aleksandr Endovitckij <endovitsky.a.p@gmail.com>
2021-05-11 18:56:12 +0300

    Add BaseComponent

---
+++ Assets/Scripts/Components/BaseComponent.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Components
+{
+    public abstract class BaseComponent : MonoBehaviour
+    {
+        private void Awake()
+        {
+            Initialize();
+
+            Subscribe();
+        }
+        private void OnDestroy()
+        {
+            UnSubscribe();
+
+            UnInitialize();
+        }
+
+        protected abstract void Initialize();
+        protected abstract void UnInitialize();
+        protected abstract void Subscribe();
+        protected abstract void UnSubscribe();
+    }
+}
