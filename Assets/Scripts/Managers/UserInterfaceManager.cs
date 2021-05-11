359777ac8ea1339dd0e3043cb17c227de19bbf21
Aleksandr Endovitckij <endovitsky.a.p@gmail.com>
2021-05-11 17:23:05 +0300

    Add UserInterfaceManager

---
+++ Assets/Scripts/Managers/UserInterfaceManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using Utils;
+
+namespace Managers
+{
+    public class UserInterfaceManager : MonoBehaviour, IInitilizable
+    {
+        public void Initialize()
+        {
+
+        }
+    }
+}
