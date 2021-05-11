35372737d002cff55a9ff466d469fa3a6d91a43b
Aleksandr Endovitckij <endovitsky.a.p@gmail.com>
2021-05-12 02:10:21 +0300

    Add ScoreManager

---
+++ Assets/Scripts/Managers/ScoreManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Models;
+using UnityEngine;
+using Utils;
+
+namespace Managers
+{
+    public class ScoreManager : MonoBehaviour, IInitilizable
+    {
+        public Action<int> ScoreChanged = delegate {  };
+
+        public int Score
+        {
+            get
+            {
+                return _score;
+            }
+            set
+            {
+                if (_score == value)
+                {
+                    return;
+                }
+
+                _score = value;
+
+                Debug.Log($"{this.GetType().Name}.{nameof(ScoreChanged)}" +
+                          $"\n {_score}");
+
+                ScoreChanged.Invoke(_score);
+            }
+        }
+
+        private int _score;
+
+        public void Initialize()
+        {
+            GameManager.Instance.PiecesManager.PieceModelRemoved += PieceModelRemoved;
+        }
+
+        private void PieceModelRemoved(PieceModel pieceModel)
+        {
+            Score++;
+        }
+    }
+}
